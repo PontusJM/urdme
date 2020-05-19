f4cd5e4aa04257acd399b4e305e95bfd8ea97cb6
Pontus Melin <pontus.melin.5559@student.uu.se>
2020-05-19 09:23:27 +0200

    profiling results

--- resultdisp.m
+++ resultdisp.m
@@ -2,45 +2,64 @@
 
 close all;
 
-% RNG impact &
+% RNG impact (task; chunk) &
 % DRAND48, RAND_R, GSL_TAUS2, GSL_MT19937, GSL_RANLXS0, GSL_RANXLS2 %
-rng =  [142.099, 58.337, 58.468, 59.577, 67.681, 112.612];
-bar(rng,0.5);
+rng =  [142.099, 150.365; 58.337, 61.884; 58.468, 64.028; 59.577, 66.5; 67.681, 78.621; 112.612, 124.408];
+bar(rng);
 ylabel('time [s]');
-title('Execution times for RNG algorithms');
+title('Execution times of RNG algorithms','FontSize',15);
 set(gca, 'XTickLabel', {'DRAND48', 'RAND\_R', 'GSL\_TAUS2', 'GSL\_MT19937', 'GSL\_RANLXS0', 'GSL\_RANLXS2'});
+ax = gca;
+ax.XAxis.FontSize = 15;
+ax.YAxis.FontSize = 15;
+legend({'Task','Transparent'},'FontSize',15);
+figure;
 
 % Task Based MT19937 performance vs. serial 12 replicas %
 % 1 2 4 6 8 10 12 %
-mt = [491.197, 267.168 , 176.913, 174.847, 162.753, 191.435, 144.203];
-bar(mt,0.5);
-ylabel('time [s]')
-title('Execution times of the task based parallel solver compared to the serial solver');
+res = [491.197 495.404; 267.168 272.454; 176.913 190.74; 174.847 185.078; 162.753 175.078; 191.435 161.318; 144.203 157.896];
+bar(res);
+ylabel('Time [s]')
+xlabel('Threads')
+title('Execution times of the different solvers running on a varying amount of threads','FontSize',15);
 set(gca,'XTickLabel',{1,2,4,6,8,10,12});
+ax = gca;
+ax.XAxis.FontSize = 15;
+ax.YAxis.FontSize = 15;
 hold on
-plot(xlim,[457.561 457.561], 'r')
-legend({'Parallel','Serial'});
+plot(xlim,[457.561 457.561], 'r','LineWidth',2.5)
+legend({'Task','Transparent','Serial'},'FontSize',15);
 
+figure;
 
 % Task Based MT19937 performance vs. serial 6 replicas %
 % 1 2 4 6 8 10 12 %
-mt = [243.456, 132.936 , 104.158 , 87.548 , 87.750, 87.350, 87.350];
-bar(mt,0.5);
-ylabel('time [s]')
-title('Execution times of the task based parallel solver compared to the serial solver');
+res = [243.456 247.845; 132.936 141.238; 104.158 96.743; 87.548 93.382; 87.750 88.726; 87.350 83.07; 87.350 80.051];
+bar(res);
+ylabel('Time [s]')
+xlabel('Threads')
+title('Execution times of the different solvers running on a varying amount of threads','FontSize',15);
 set(gca,'XTickLabel',{1,2,4,6,8,10,12});
+ax = gca;
+ax.XAxis.FontSize = 15;
+ax.YAxis.FontSize = 15;
 hold on
-plot(xlim,[221.623 221.623], 'r')
-legend({'Parallel','Serial'});
+plot(xlim,[221.623 221.623], 'r','LineWidth',2.5)
+legend({'Task','Transparent','Serial'},'FontSize',15);
 
+figure;
 
 % Task Based MT19937 performance vs. serial 1 replica %
 % 1 2 4 6 8 10 12 %
-mt = [40.587, 40.691 , 40.697, 40.675, 40.614, 40.595 , 40.578];
-bar(mt,0.5);
-ylabel('time [s]')
-title('Execution times of the task based parallel solver compared to the serial solver');
+res = [40.587 42.727; 40.691 24.042; 40.697 15.891; 40.675 15.056; 40.614 14.669; 40.595 13.846; 40.578 13.106];
+bar(res);
+ylabel('Time [s]')
+xlabel('Threads')
+title('Execution times of the different solvers running on a varying amount of threads','FontSize',15);
 set(gca,'XTickLabel',{1,2,4,6,8,10,12});
+ax = gca;
+ax.XAxis.FontSize = 15;
+ax.YAxis.FontSize = 15;
 hold on
-plot(xlim,[39.578 39.578], 'r')
-legend({'Parallel','Serial'});
+plot(xlim,[39.578 39.578], 'r','LineWidth',2.5)
+legend({'Task','Transparent','Serial'},'FontSize',15);
